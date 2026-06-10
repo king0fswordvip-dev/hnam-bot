03c034ffbd5bc62b312f685987163fd512f2acdb
king0fswordvip-dev <king0fswordvip@gmail.com>
2026-06-10 07:44:58 +0700

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,5 @@
+-- MAIN LOADER
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/king0fswordvip-dev/hnam-bot/main/esp.lua"))()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/king0fswordvip-dev/hnam-bot/main/ShootAll.lua"))()

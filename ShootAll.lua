a8baa36919ed1f52149d68d38dd918f49699ee45
king0fswordvip-dev <king0fswordvip@gmail.com>
2026-06-10 07:36:31 +0700

    Update ShootAll.lua

--- ShootAll.lua
+++ ShootAll.lua
@@ -4,15 +4,7 @@ local ShootAllEvent = Instance.new("RemoteEvent")
 ShootAllEvent.Name = "ShootAllEvent"
 ShootAllEvent.Parent = ReplicatedStorage
 
-local Admins = {
-	[123456789] = true, -- thay bằng UserId của bạn
-}
-
 ShootAllEvent.OnServerEvent:Connect(function(Player)
-
-	if not Admins[Player.UserId] then
-		return
-	end
 
 	for _, Target in ipairs(game.Players:GetPlayers()) do
 
@@ -21,13 +13,11 @@ ShootAllEvent.OnServerEvent:Connect(function(Player)
 			local Character = Target.Character
 
 			if Character then
-
 				local Humanoid = Character:FindFirstChildOfClass("Humanoid")
 
 				if Humanoid then
 					Humanoid.Health = 0
 				end
-
 			end
 
 		end
